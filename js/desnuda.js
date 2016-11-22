874db6b756e73dac320f0d73b9c88f7b9df0f1b1
Shanda E <shanda.e.clark@gmail.com>
2016-11-22 02:48:19 +0000

    added html css js images to all

---
+++ js/desnuda.js
@@ -0,0 +1,21 @@
+function form_submit() {
+    document.getElementById("contactform").submit();
+   }    
+   /* This is the code to make the "send message" button in modal contact form disappear.*/
+
+
+
+
+
+
+$(document).ready(function(){
+    $('#move').click(function(){
+        $('#move').animate({left: '500px'});
+    });
+});
+
+
+$(document).ready(function(){
+    $('#move').click(function(){
+        $('#move').effect('bounce',{times:3}, 500);});
+    });
